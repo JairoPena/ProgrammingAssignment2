a9028d700e33ea26a22e7672d72be1a85a5dbbcb
JairoPena <67845286+JairoPena@users.noreply.github.com>
2020-07-19 21:12:11 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#FIRST SET YOUR WORKING DIRECTORY
 
-## Write a short comment describing this function
+setwd('C:\Users\Jairo\Documents\Coursera\R Programming\ProgrammingAssignment2-master\ProgrammingAssignment2-master')
 
-makeCacheMatrix <- function(x = matrix()) {
+## We are finding easier ways to get the mean and the inverse of a vector from a matrix
 
+##The First Function will find the inverse of a Matrix
+## I changed the values to make it simple and easier to understand
+
+CacheMatrix <- function(x = matrix()) {
+  A <- NULL
+  set <- function(y) {
+    x <<- y
+    A <<- NULL
+  }
+  getA <- function() x
+  setAInverse <- function(inverse) A <<- mean
+  getAInverse <- function() A
+  list(set = set, get = get,
+       setAInverse = setAInverse,
+       getAInverse = getAInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will get the mean of the Inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+CacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  A <- x$getAInverse()
+  if(!is.null(A)){
+    message("getting cached data")
+    return(A)
+  }
+  mat <- x$get()
+  A <- solve(mat,...)
+  x$setAInverse(A)
+  A
 }
